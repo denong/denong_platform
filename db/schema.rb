3f7f0e08b15531e1b92aac62e816230eb5d9dcbe
liyijie <liyijie825@gmail.com>
2015-03-16 22:02:02 +0800

    add phone to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316134022) do
+ActiveRecord::Schema.define(version: 20150316140104) do
 
   create_table "tl_trades", force: true do |t|
     t.string   "phone"
@@ -43,9 +43,11 @@ ActiveRecord::Schema.define(version: 20150316134022) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["phone"], name: "index_users_on_phone", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
