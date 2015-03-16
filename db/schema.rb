34757c6bfa610996356ae910b321236aa3e7a872
liyijie <liyijie825@gmail.com>
2015-03-16 22:09:22 +0800

    add user comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316140231) do
+ActiveRecord::Schema.define(version: 20150316140808) do
 
   create_table "tl_trades", force: true do |t|
     t.string   "phone"
@@ -44,8 +44,10 @@ ActiveRecord::Schema.define(version: 20150316140231) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "phone"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email"
   add_index "users", ["phone"], name: "index_users_on_phone", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
