b4340bddc86b3c6ffaee58f976621fef17fb1adb
yuzixun <yzxun@qq.com>
2015-04-09 22:19:20 +0800

    add merchant giving_logs

--- app/models/merchant_giving_log.rb
+++ app/models/merchant_giving_log.rb
@@ -16,4 +16,17 @@ class MerchantGivingLog < ActiveRecord::Base
   belongs_to :customer
   has_one :jajin_log, as: :jajinable
   
+  def as_json(options=nil)
+    {
+      id: id,
+      amount: amount,
+      giving_time: giving_time,
+      merchant_id: merchant_id,
+      customer_id: customer_id,
+      created_at: created_at,
+      updated_at: updated_at,
+      given: ""
+    }
+  end
+
 end
