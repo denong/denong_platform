780d3ef33506338ae259563ca92b81b8949ccf14
yuzixun <yzxun@qq.com>
2015-04-10 22:53:07 +0800

    add upload identity verifies failed

--- spec/acceptance/identity_verifies_controller_spec.rb
+++ spec/acceptance/identity_verifies_controller_spec.rb
@@ -9,7 +9,6 @@ resource "用户信息验证" do
     before do
       FactoryGirl.create(:customer_with_jajin_pension)
       @front_image = FactoryGirl.create(:image, photo_type: "front")
-      # @back_image = create(:image, photo_type: "back")
     end
 
     parameter :name, "用户名称", required: true, scope: :identity_verify
@@ -30,15 +29,56 @@ resource "用户信息验证" do
     response_field :back_image, "身份证反面照片"
 
     let(:name) { "test" }
-    let(:id_num) { "13941293487192347" }
+    let(:id_num) { "333333333333333333" }
     let(:front_image) { "[uploaded data]" }
     let(:back_image) { "[uploaded data]" }
     let(:raw_post) { params.to_json }
 
     example "上传身份证成功" do
       do_request
-      # puts "response is #{response_body}"
       expect(status).to eq 200
+    end
+
+    example "上传身份证失败" do
+      do_request
+      puts "response is #{response_body}"
+      expect(status).to eq 200
+    end
+  end
+
+  post "/identity_verifies" do
+    before do
+      create(:customer_with_jajin_pension)
+      @front_image = FactoryGirl.create(:image, photo_type: "front")
+      create(:identity_verify)
+    end
+
+    parameter :name, "用户名称", required: true, scope: :identity_verify
+    parameter :id_num, "身份证号码", required: true, scope: :identity_verify
+    parameter :front_image, "正面照片",required: true, scope: :identity_verify
+    parameter :back_image, "反面照片",required: true, scope: :identity_verify
+
+
+    user_attrs = FactoryGirl.attributes_for(:user)
+
+    header "X-User-Token", user_attrs[:authentication_token]
+    header "X-User-Phone", user_attrs[:phone]
+
+    response_field :name, "姓名"
+    response_field :id_num, "身份证号码"
+    response_field :verify_state, "验证状态"
+    response_field :front_image, "身份证正面照片"
+    response_field :back_image, "身份证反面照片"
+
+    let(:name) { "test" }
+    let(:id_num) { "333333333333333333" }
+    let(:front_image) { "[uploaded data]" }
+    let(:back_image) { "[uploaded data]" }
+    let(:raw_post) { params.to_json }
+
+    example "上传身份证失败" do
+      do_request
+      expect(status).to eq 422
     end
   end
 
