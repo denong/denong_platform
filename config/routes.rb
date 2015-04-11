053292e345c80f240b34f4a589c983ad0040d752
liyijie <liyijie825@gmail.com>
2015-04-11 22:30:16 +0800

    merchant_sys_reg_info use resource instead of resources

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :merchant_sys_reg_info
+  resource :merchant_sys_reg_info
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
