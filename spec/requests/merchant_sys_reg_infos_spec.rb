a3bf4dd43780791596156c58d8b9260b97ca2d07
liyijie <liyijie825@gmail.com>
2015-04-11 22:42:44 +0800

    rspec error fix

--- spec/requests/merchant_sys_reg_infos_spec.rb
+++ spec/requests/merchant_sys_reg_infos_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "MerchantSysRegInfos", type: :request do
-  describe "GET /merchant_sys_reg_infos" do
-    it "works! (now write some real specs)" do
-      get merchant_sys_reg_infos_path
-      expect(response).to have_http_status(200)
-    end
-  end
+
 end
--- spec/routing/merchant_sys_reg_infos_routing_spec.rb
+++ spec/routing/merchant_sys_reg_infos_routing_spec.rb
@@ -3,32 +3,28 @@ require "rails_helper"
 RSpec.describe MerchantSysRegInfosController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/merchant_sys_reg_infos").to route_to("merchant_sys_reg_infos#index")
-    end
-
     it "routes to #new" do
-      expect(:get => "/merchant_sys_reg_infos/new").to route_to("merchant_sys_reg_infos#new")
+      expect(:get => "/merchant_sys_reg_info/new").to route_to("merchant_sys_reg_infos#new")
     end
 
     it "routes to #show" do
-      expect(:get => "/merchant_sys_reg_infos/1").to route_to("merchant_sys_reg_infos#show", :id => "1")
+      expect(:get => "/merchant_sys_reg_info").to route_to("merchant_sys_reg_infos#show")
     end
 
     it "routes to #edit" do
-      expect(:get => "/merchant_sys_reg_infos/1/edit").to route_to("merchant_sys_reg_infos#edit", :id => "1")
+      expect(:get => "/merchant_sys_reg_info/edit").to route_to("merchant_sys_reg_infos#edit")
     end
 
     it "routes to #create" do
-      expect(:post => "/merchant_sys_reg_infos").to route_to("merchant_sys_reg_infos#create")
+      expect(:post => "/merchant_sys_reg_info").to route_to("merchant_sys_reg_infos#create")
     end
 
     it "routes to #update" do
-      expect(:put => "/merchant_sys_reg_infos/1").to route_to("merchant_sys_reg_infos#update", :id => "1")
+      expect(:put => "/merchant_sys_reg_info").to route_to("merchant_sys_reg_infos#update")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/merchant_sys_reg_infos/1").to route_to("merchant_sys_reg_infos#destroy", :id => "1")
+      expect(:delete => "/merchant_sys_reg_info").to route_to("merchant_sys_reg_infos#destroy")
     end
 
   end
