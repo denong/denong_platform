092b9218306f953a60877118ecb2ad033531cbd6
liyijie <liyijie825@gmail.com>
2015-04-28 16:37:54 +0800

    fetch jajin log by current customer

--- app/controllers/jajin_logs_controller.rb
+++ app/controllers/jajin_logs_controller.rb
@@ -4,11 +4,11 @@ class JajinLogsController < ApplicationController
   acts_as_token_authentication_handler_for User
 
   def index
-    @jajin_logs = JajinLog.all.paginate(page: params[:page], per_page: 10)
+    @jajin_logs = current_customer.jajin_logs.paginate(page: params[:page], per_page: 10)
     respond_with(@jajin_logs)
   end
 
   def show
-    @jajin_log = JajinLog.find params[:id]
+    @jajin_log = current_customer.jajin_logs.find params[:id]
   end
 end
