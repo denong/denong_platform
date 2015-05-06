d22c7bda10b9867364392cd69bd6983163f4a9d2
yuzixun <yzxun@qq.com>
2015-05-06 23:15:01 +0800

    delete the no need code

--- spec/views/topics/new.html.erb_spec.rb
+++ spec/views/topics/new.html.erb_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "topics/new", type: :view do
-  before(:each) do
-    assign(:topic, Topic.new())
-  end
 
-  it "renders new topic form" do
-    render
-
-    assert_select "form[action=?][method=?]", topics_path, "post" do
-    end
-  end
 end
--- spec/views/topics/show.html.erb_spec.rb
+++ spec/views/topics/show.html.erb_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "topics/show", type: :view do
-  before(:each) do
-    @topic = assign(:topic, Topic.create!())
-  end
 
-  it "renders attributes in <p>" do
-    render
-  end
 end
