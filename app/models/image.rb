063aa40add512d5d188e36c723106d7355c1b05e
liyijie <liyijie825@gmail.com>
2015-05-25 13:21:27 +0800

    update image content_type to image/all

--- app/models/image.rb
+++ app/models/image.rb
@@ -22,5 +22,5 @@ class Image < ActiveRecord::Base
 
   validates_attachment_presence :photo
   validates_attachment_size     :photo, less_than: 5.megabytes
-  validates_attachment_content_type :photo, content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
