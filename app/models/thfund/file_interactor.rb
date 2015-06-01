10fcd152ee9d0f4cacfd771510185f802c64c1ad
liyijie <liyijie825@gmail.com>
2015-06-01 11:29:35 +0800

    add write_attribute method

--- app/models/thfund/file_interactor.rb
+++ app/models/thfund/file_interactor.rb
@@ -1,6 +1,12 @@
 module Thfund
   class FileInteractor
-    def self.read_attribute_hash(type,  content, attribute, index)
+
+    # 读取字符串其中一个属性
+    # type: 业务类型
+    # content: 一行字符串的内容
+    # attribute: 属性的名称
+    # index: 偏移
+    def self.read_attribute(type, content, attribute, index)
 
       # 读取配置文件
       attribute_desc = ThfundSettings.send(type).send(attribute)
@@ -24,6 +30,29 @@ module Thfund
 
       # 返回相关的值
       [value, index]
+    end
+
+    # 把一个属性的值写到字符串中（append）
+    # type: 业务类型
+    # content: 一行字符串的内容
+    # attribute: 属性的名称
+    # value: 属性值
+    def self.write_attribute(type, content, attribute, value)
+      # 读取配置文件
+      attribute_desc = ThfundSettings.send(type).send(attribute)
+      attribute_type = attribute_desc[0]
+      attribute_length = attribute_desc[1]
+
+      # 根据配置类型，将value的值写到content中
+      case attribute_type.upcase
+      when "C"
+        content = "#{content}#{value.to_s.ljust(attribute_length, ' ')}"
+      when "A"
+        content = "#{content}#{value.to_s.rjust(attribute_length, '0')}"
+      when "N"
+        value = value.to_f * (10 ** float_ind)
+        content = "#{content}#{value.to_i.to_s.rjust(attribute_length, '0')}"
+      end
     end
   end
 end
