1b3ac403e12a8e3bc8520b0fe2509c0f30d1b41b
liyijie <liyijie825@gmail.com>
2015-07-06 19:49:35 +0800

    bug fix

--- app/models/thfund_account.rb
+++ app/models/thfund_account.rb
@@ -55,8 +55,9 @@ class ThfundAccount < ActiveRecord::Base
   end
 
   def self.create_by_customer customer
-    account = ThfundAccount.find_or_initialize_by customer: customer
+    account = ThfundAccount.new
 
+    account.customer = customer
     account.mobile = customer.try(:user).try(:phone)
     account.name = customer.try(:customer_reg_info).try(:name)
     account.certification_no = customer.try(:customer_reg_info).try(:id_card)
