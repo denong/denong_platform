a30fc483b58daa206f0cbe5a548c183ea4b484ec
yuzixun <yzxun@qq.com>
2015-07-30 11:31:05 +0800

    add bank card search

---
+++ app/models/bank_card_search.rb
@@ -0,0 +1,23 @@
+class BankCardSearch
+  def self.search q = {}
+    # These fields are special
+    page = q.delete(:page) || 1
+    per_page = q.delete(:per_page) || 10
+
+    search_text = q[:search] || ""
+    order_sym = q[:order].try(:to_sym)
+
+    # Perform the search
+    s = BankCard.search do
+      paginate page: page, per_page: per_page
+      
+      fulltext search_text if search_text.present?
+
+      # if order_sym == :votes_up
+      #   order_by :votes_up, :desc
+      # end
+    end
+
+    s.results
+  end
+end
---
+++ app/models/verify_process.rb
@@ -0,0 +1,18 @@
+class VerifyProcess < ActiveRecord::Base
+
+
+  def verify_info
+    #调用接口
+    #response
+    # if condition
+        # BankCard.add_bank_card params
+        # IdentityVerify.verified!
+        # IdentityVerify.processing
+    # else
+        # IdentityVerify.verified_fail!
+    # end
+  end
+
+
+
+end
