a2ee2bb639800a742a3611726771ad537a9ce957
yuzixun <yzxun@qq.com>
2015-07-30 12:55:32 +0800

    add process of member card

--- app/models/member_card.rb
+++ app/models/member_card.rb
@@ -15,13 +15,14 @@
 
 class MemberCard < ActiveRecord::Base
   # include ActionView::Helpers::AssetUrlHelper
-  
+  # 由于需求变动，passwd改成身份证号，user_name改成名字
+
   belongs_to :merchant
   belongs_to :customer
 
   has_many :member_card_point_logs
 
-  validates_uniqueness_of :user_name, scope: :merchant_id
+  validates_uniqueness_of :passwd, scope: :merchant_id
   validate :authenticate, on: :create
 
   after_create :add_merchant_member_card_amount
@@ -39,22 +40,47 @@ class MemberCard < ActiveRecord::Base
   end
 
   private
-  
-    def authenticate
-      #以后与其他商家对接,以下代码暂时用于内部测试
-      member_card = MerchantCustomer.find_by(u_id: user_name)
-      if member_card.nil?
-        errors.add(:message, "会员卡不存在")
-        return
-      end
 
-      if member_card.password != passwd
-        puts "密码错误"
-        errors.add(:message, "密码错误")
-        return
+    def authenticate
+      if (passwd =~ /^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/ && idcard_verify?)
+        return true
+      else
+        errors.add(:message, "用户信息不正确")
+        return false
       end
-      self.point = member_card.jifen
     end
+
+    def idcard_verify?
+      personal_info = PersonalInfo.find_by_id_card(passwd)
+      if personal_info.present? && personal_info.name == user_name
+        
+        return true
+      end
+      response = RestClient.get 'http://apis.haoservice.com/idcard/VerifyIdcard', {params: {cardNo: passwd, realName: user_name, key: "0e7253b6cf7f46088c18a11fdf42fd1b"}}
+      response_hash = MultiJson.load(response)
+      if response_hash["error_code"].to_i == 0
+        response_hash["result"]["isok"]
+        PersonalInfo.find_or_create_by(name: user_name, id_card: passwd)
+      else
+        false
+      end
+    end
+
+    # def old_authenticate
+    #   #以后与其他商家对接,以下代码暂时用于内部测试
+    #   member_card = MerchantCustomer.find_by(u_id: user_name)
+    #   if member_card.nil?
+    #     errors.add(:message, "会员卡不存在")
+    #     return
+    #   end
+
+    #   if member_card.password != passwd
+    #     puts "密码错误"
+    #     errors.add(:message, "密码错误")
+    #     return
+    #   end
+    #   self.point = member_card.jifen
+    # end
 
     def add_merchant_member_card_amount
       merchant.member_card_amount += 1
