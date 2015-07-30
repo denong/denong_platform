791bf508fba11c632ec792bd9dbcc4404d3fea19
yuzixun <yzxun@qq.com>
2015-07-30 15:28:49 +0800

    bug fix

--- app/models/member_card.rb
+++ app/models/member_card.rb
@@ -58,8 +58,10 @@ class MemberCard < ActiveRecord::Base
       response = RestClient.get 'http://apis.haoservice.com/idcard/VerifyIdcard', {params: {cardNo: passwd, realName: user_name, key: "0e7253b6cf7f46088c18a11fdf42fd1b"}}
       response_hash = MultiJson.load(response)
       if response_hash["error_code"].to_i == 0
+        if response_hash["result"]["isok"]
+          PersonalInfo.find_or_create_by(name: name, id_card: id_card)
+        end
         response_hash["result"]["isok"]
-        PersonalInfo.find_or_create_by(name: user_name, id_card: passwd)
       else
         false
       end
