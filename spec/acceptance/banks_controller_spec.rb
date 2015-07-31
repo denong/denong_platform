901a1590389acce2236422157017649ed34f6b2e
yuzixun <yzxun@qq.com>
2015-07-31 15:15:48 +0800

    add the bank_card_amount when response

--- spec/acceptance/banks_controller_spec.rb
+++ spec/acceptance/banks_controller_spec.rb
@@ -17,7 +17,7 @@ resource "银行" do
       Bank.reindex
       Sunspot.commit
 
-      create(:bank_card, customer: customer, bank_name: "招商银行", bank_id: 8)
+      create(:bank_card, customer: customer, bank_name: "招商银行", bank_id: Bank.first.id)
     end
 
     user_attrs = FactoryGirl.attributes_for(:user)
@@ -27,6 +27,11 @@ resource "银行" do
     response_field :total_pages, "总页数"
     response_field :current_page, "页码"
     response_field :name, "银行名称"
+    response_field :id, "银行id"
+    response_field :bind_bank_card, "是否已经授权"
+    response_field :logo, "银行logo"
+    response_field :bank_card_amount, "已授权的银行卡数量"
+
 
     parameter :page, "页码", required: false
     parameter :search, "搜索字段", required: false
@@ -35,6 +40,7 @@ resource "银行" do
 
     example "搜索银行名称" do
       do_request
+      puts "response is #{response_body}"
       expect(status).to eq(200)
     end
 
