b16957e4ece631f1ba3c0ccacba2090780dc942b
shtzr840329 <shtzr840329@gmail.com>
2015-08-06 21:58:46 +0800

    fix ssl

--- app/models/bank_card.rb
+++ app/models/bank_card.rb
@@ -177,7 +177,7 @@ class BankCard < ActiveRecord::Base
     signature = signature.delete("\n")
     signature = CGI.escape(signature)
 
-    conn = Faraday.new(:url => "#{dq_base_url}", { ssl: { verify: false } }) do |faraday|
+    conn = Faraday.new(:url => "#{dq_base_url}", :ssl => { :verify => false } ) do |faraday|
     # conn = Faraday.new(:url => "#{dq_base_url}", :ssl => {:ca_path => "/usr/lib/ssl/certs" }) do |faraday|
       faraday.request  :url_encoded
       faraday.response :logger
