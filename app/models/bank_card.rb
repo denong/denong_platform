b1ae0452b8039a6c1b2935f0ab9f41a464883183
shtzr840329 <shtzr840329@gmail.com>
2015-08-07 13:10:58 +0800

    test encode

--- app/models/bank_card.rb
+++ app/models/bank_card.rb
@@ -180,6 +180,7 @@ class BankCard < ActiveRecord::Base
       faraday.adapter  Faraday.default_adapter
     end
     # v_params = CGI.escape(v_params)
+    v_params = v_params.encode('utf-8')
     v_params = URI::encode(v_params)
     p v_params
     request_params = "data=#{v_params}&sign=#{signature}&sign_type=RSA&version=1.0"
