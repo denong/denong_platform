a8df82a16a97049157165edd3bf940c015e64b9d
shtzr840329 <shtzr840329@gmail.com>
2015-08-07 17:08:28 +0800

    test encode

--- app/models/bank_card.rb
+++ app/models/bank_card.rb
@@ -162,7 +162,7 @@ class BankCard < ActiveRecord::Base
     v_params.bp_id = "998800001145881"
     v_params.api_key = "real_7788000013635914866"
     v_params.bp_order_id = Time.zone.now.strftime("%Y%m%d%H%M%S")
-    name = "于子洵".encode('utf-8')
+    name = "于子洵".force_encoding('utf-8')
     v_params.user_name = CGI.escape(name)
     p v_params.user_name
     v_params.cert_type = "a"
