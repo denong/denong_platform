550fd9efdd239eb503e06367935faf4339f75cec
shtzr840329 <shtzr840329@gmail.com>
2015-08-11 12:13:41 +0800

    add model scope

--- app/models/merchant_customer.rb
+++ app/models/merchant_customer.rb
@@ -7,7 +7,7 @@
 #  password    :string(255)      default(""), not null
 #  name        :string(255)
 #  phone       :string(255)
-#  jifen       :float
+#  jifen       :float(24)
 #  is_changed  :integer
 #  merchant_id :integer
 #  created_at  :datetime
@@ -23,5 +23,8 @@ class MerchantCustomer < ActiveRecord::Base
   belongs_to :merchant
   belongs_to :customer
 
+  scope :today, -> { where('created_at > ?', Time.zone.now.to_date - 1.day) }
+  scope :week, -> { where('created_at > ?', Time.zone.now.to_date - 7.day ) }
+  scope :month, -> { where('created_at > ?', Time.zone.now.to_date - 30.day ) }
 
 end
