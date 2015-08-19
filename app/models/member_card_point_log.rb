ed5947bb1bf3d1fc4124ad9484655ae4cd74e7e8
yuzixun <yzxun@qq.com>
2015-08-19 15:07:39 +0800

    bug fix`

--- app/models/member_card_point_log.rb
+++ app/models/member_card_point_log.rb
@@ -25,7 +25,7 @@ class MemberCardPointLog < ActiveRecord::Base
   validates_presence_of :member_card, on: :create
 
   validate :must_point_negative, on: :create
-  # validate :must_have_jajin, on: :create
+  validate :must_have_jajin, on: :create
   validate :point_must_less_than_all_point, on: :create
 
   default_scope { order('id DESC') }
@@ -75,6 +75,13 @@ class MemberCardPointLog < ActiveRecord::Base
       unless point < 0
         errors.add(:point, "必须小于0")
       end
+    end
+
+    def must_have_jajin
+      if self.customer.try(:jajin).blank?
+        errors.add(:message, "小确幸账号不存在")
+      end
+      self.jajin = point.abs
     end
 
     def point_must_less_than_all_point
