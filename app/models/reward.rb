89a12f8394aff2c581f5b2b2342193d551561b13
shtzr840329 <shtzr840329@gmail.com>
2015-08-20 17:07:49 +0800

    fix reward

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -30,7 +30,7 @@ class Reward < ActiveRecord::Base
     def generate_verify_code
       # self.verify_code ||= Devise.friendly_token.first(10)
       # self.verify_code ||= (0..9).to_a.sample(8).join
-      chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a)
+      chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
       self.verify_code ||= chars.sample(8).join
     end
 
