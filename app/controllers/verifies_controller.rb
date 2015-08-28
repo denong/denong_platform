c54dfaac4784e7006a5e36b034b9f7af0549565c
yuzixun <yzxun@qq.com>
2015-08-28 18:01:18 +0800

    modified from create to find_or_create_by params:

--- app/controllers/verifies_controller.rb
+++ app/controllers/verifies_controller.rb
@@ -5,11 +5,10 @@ class VerifiesController < ApplicationController
 
   def show
     if JajinVerifyLog.tl_varify params
-      puts "params is #{params}"
       init_params = {}
       init_params[:verify_code] = params[:ckh]
       init_params[:amount] = params[:amt]
-      @jajin_verify_log = current_customer.jajin_verify_logs.create init_params
+      @jajin_verify_log = current_customer.jajin_verify_logs.find_or_create_by(init_params)
     end
 
   end
