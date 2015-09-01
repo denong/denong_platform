4ea76cdda81779218ba72e71e8dfda89243f7372
yuzixun <yzxun@qq.com>
2015-09-01 19:06:58 +0800

    optimize

--- app/models/tl_trade.rb
+++ app/models/tl_trade.rb
@@ -75,9 +75,9 @@ class TlTrade < ActiveRecord::Base
   end
 
   def check_pos_machine
-    pos_machine = PosMachine.find_by_pos_ind(pos_ind)
+    pos_machine = PosMachine.where(pos_ind: pos_ind+shop_ind)
     if pos_machine.nil?
-      pos_machine = PosMachine.create pos_ind: pos_ind
+      pos_machine = PosMachine.create pos_ind: (pos_ind+shop_ind)
     end
     self.pos_machine = pos_machine
     self.shop = pos_machine.shop
