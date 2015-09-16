82f376a88317a0ecfb0d2c42c3f6c071aa33eede
shtzr840329 <shtzr840329@gmail.com>
2015-09-16 10:20:22 +0800

    fix

--- app/models/member_card_point_log.rb
+++ app/models/member_card_point_log.rb
@@ -167,7 +167,6 @@ class MemberCardPointLog < ActiveRecord::Base
     rescue Exception => e
       p e
     end
-    # MemberCardPointLog.process_data_from_cache key
   end
 
   def self.get_point_log_by_merchant merchant_id, params
