98d162e429cc2b1978480c37d4c25417d9081d76
yuzixun <yzxun@qq.com>
2015-09-23 15:51:02 +0800

    add export data method

--- app/models/log_process.rb
+++ app/models/log_process.rb
@@ -19,4 +19,26 @@ class LogProcess
     end
     "#{logs_folder}/#{key}.xlsx"
   end
+
+  def self.export_member_card_log start_time, end_time
+    logs = MemberCardPointLog.where(created_at: start_time..end_time)
+    logs_folder = File.join("public", "logs", "#{Time.now.to_date}")
+    FileUtils.makedirs(logs_folder) unless File.exist?(logs_folder)
+    file = Axlsx::Package.new
+    file.workbook.add_worksheet(:name => "sheet1") do |sheet|
+      sheet.add_row ["姓名", "手机号", "身份证", "小金", "兑换时间"]
+
+      logs.each do |log|
+        name = log.try(:customer).try(:customer_reg_info).try(:name)
+        phone = log.try(:customer).try(:user).try(:phone)
+        id_card = log.try(:customer).try(:customer_reg_info).try(:id_card)
+        point = log.try(:jajin)
+        time = log.try(:created_at).strftime("%Y%m%d%H%M%S")
+        sheet.add_row([name, phone, id_card, point, time], :types => [:string, :string, :string, :string, :string, :string])
+      end
+      file.use_shared_strings = true
+      file.serialize("#{logs_folder}/#{start_time}到#{end_time}.xlsx")
+    end
+    "#{logs_folder}/#{start_time}到#{end_time}.xlsx"
+  end
 end
