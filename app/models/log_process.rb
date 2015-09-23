8c3d8cc64b205ca8086cc533519ac30dc2a3658d
yuzixun <yzxun@qq.com>
2015-09-23 17:36:37 +0800

    add export tl trade method

--- app/models/log_process.rb
+++ app/models/log_process.rb
@@ -41,4 +41,33 @@ class LogProcess
     end
     "#{logs_folder}/#{start_time}到#{end_time}.xlsx"
   end
+
+
+  def self.export_tl_trade start_time, end_time
+    logs = TlTrade.where(created_at: start_time..end_time)
+    logs_folder = File.join("public", "logs", "#{Time.now.to_date}")
+    FileUtils.makedirs(logs_folder) unless File.exist?(logs_folder)
+    file = Axlsx::Package.new
+    filename = "#{start_time.strftime("%m%d")}到#{end_time.strftime("%m%d")}"
+    file.workbook.add_worksheet(:name => "sheet1") do |sheet|
+      sheet.add_row ["商户号", "终端号", "交易日期", "交易时间", "交易金额", "手机号", "交易卡号", "姓名"]
+
+      logs.each do |log|
+        next if log.try(:customer).try(:customer_reg_info).try(:verify_state) != 2
+
+        shop_ind = log.try(:shop_ind)
+        pos_ind = log.try(:pos_ind)
+        trade_date = log.try(:trade_time)[0..7]
+        trade_time = log.try(:trade_time)[-6..-1]
+        price = log.try(:price)
+        phone = log.try(:phone)
+        card = log.try(:card)
+        name = log.try(:customer).try(:customer_reg_info).try(:name)
+        sheet.add_row([name, phone, id_card, point, time], :types => [:string, :string, :string, :string, :string, :string])
+      end
+      file.use_shared_strings = true  
+      file.serialize("#{logs_folder}/#{filename}.xlsx")
+    end
+    return "#{logs_folder}/#{filename}.xlsx", logs.size
+  end
 end
