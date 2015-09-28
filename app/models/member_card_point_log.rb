57cd2f69eea73eabdfc79ac687c15f782cf42567
shtzr840329 <shtzr840329@gmail.com>
2015-09-28 11:21:46 +0800

    fix thread pool

--- app/models/member_card_point_log.rb
+++ app/models/member_card_point_log.rb
@@ -11,6 +11,7 @@
 #  member_card_id :integer
 #  unique_ind     :string(255)
 #
+require 'thread/pool'
 
 class MemberCardPointLog < ActiveRecord::Base
   belongs_to :customer
@@ -70,7 +71,7 @@ class MemberCardPointLog < ActiveRecord::Base
     datas = $redis.hvals("#{key}")
     error_logs = []
     merchant = Merchant.find_by(id: 162)
-    pool = ThreadPool.new(30)
+    pool = Thread.pool(50)
     count = 0
     datas.each do |data|
       begin
@@ -80,9 +81,7 @@ class MemberCardPointLog < ActiveRecord::Base
       end
       data[:merchant_id] = 162
       
-      pool.process do
-        process_one_data data, datetime
-      end
+      pool.process { process_one_data data, datetime }
       count += 1
       p "------------------------------------------------- #{count} ----------------------------------------------" 
       sleep 0.01
