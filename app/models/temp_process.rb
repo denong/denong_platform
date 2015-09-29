97e364bc073a66c42966113e47b43263935d8377
shtzr840329 <shtzr840329@gmail.com>
2015-09-29 17:45:17 +0800

    fix

--- app/models/temp_process.rb
+++ app/models/temp_process.rb
@@ -2,8 +2,7 @@ class TempProcess
 
   def self.process
     keys = ["error_logs_201509282137", "error_logs_201509290855", "error_logs_201509290856"]
-    keys.each do |i|
-      key = keys[i]
+    keys.each do |key|
       items = $redis.hgetall(key)
       items.map do |i, e|
         content = e.gsub(/\s:merchant=>.*?>,/, '')
