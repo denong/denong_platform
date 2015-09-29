d943df9df2c7a07250f897ab47939f883089e138
shtzr840329 <shtzr840329@gmail.com>
2015-09-29 17:43:24 +0800

    fix

--- app/models/temp_process.rb
+++ app/models/temp_process.rb
@@ -1,12 +1,15 @@
 class TempProcess
 
   def self.process
-    keys = ["error_logs_201509282110", "error_logs_201509282137", "error_logs_201509290855", "error_logs_201509290856"]
-    items = $redis.hgetall(keys[0])
-    items.map do |i, e|
-      content = e.gsub(/\s:merchant=>.*?>,/, '')
-      p "#{i} ---> #{content}"
-      $redis.hset(keys[0], i, content)
+    keys = ["error_logs_201509282137", "error_logs_201509290855", "error_logs_201509290856"]
+    keys.each do |i|
+      key = keys[i]
+      items = $redis.hgetall(key)
+      items.map do |i, e|
+        content = e.gsub(/\s:merchant=>.*?>,/, '')
+        p "#{i} ---> #{content}"
+        $redis.hset(key, i, content)
+      end
     end
     nil
   end
