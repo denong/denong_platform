3dce0fc42628dd66b7e22387a351c522512b4f89
shtzr840329 <shtzr840329@gmail.com>
2015-09-30 14:14:17 +0800

    fix

--- app/models/member_card_point_log.rb
+++ app/models/member_card_point_log.rb
@@ -57,7 +57,7 @@ class MemberCardPointLog < ActiveRecord::Base
   def self.data_verify hash
     params_array = []
     hash.to_a.each do |param|
-      next if param.include?(["sign", "actions", "controller"])
+      next if ["sign", "actions", "controller"].include? param
       params_array << param.join
     end
 
