023c307c203452a58a523b32a40942fd788bc881
yuzixun <yzxun@qq.com>
2015-10-16 10:43:28 +0800

    optimize

--- app/controllers/check_users_controller.rb
+++ app/controllers/check_users_controller.rb
@@ -7,7 +7,7 @@ class CheckUsersController < ApplicationController
     phone = params[:phone]
     @check_user = User.exists? phone: phone
     # 部分用户做特殊处理
-    @reset = User.exists? phone: phone, source_id: 28
+    @reset = User.exists? phone: phone, source_id: 28, sign_in_count: 0
   end
 
   def reset
