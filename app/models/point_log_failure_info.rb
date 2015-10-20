c3ac5b293c899c5be24525b000d59a1f1cfaedca
yuzixun <yzxun@qq.com>
2015-10-20 18:43:34 +0800

    modified the method

--- app/models/point_log_failure_info.rb
+++ app/models/point_log_failure_info.rb
@@ -16,4 +16,25 @@
 
 class PointLogFailureInfo < ActiveRecord::Base
   belongs_to :merchant
+
+
+  # PointLogFailureInfo.create_telecom_user_by_data
+  def self.create_telecom_user_by_data
+    errors = []
+    PointLogFailureInfo.where(error_code: "10003").each do |p|
+      next unless p.phone.present?
+      if p.point.to_i.abs < 1000
+        point = 500
+      elsif p.point.to_i.abs >= 1000
+        point = 1000
+      end
+      begin
+        TelecomUser.create(phone: p.phone, name: p.name, id_card: p.id_card, point: point, unique_ind: p.unique_ind)
+      rescue Exception => e
+        puts "exception is #{e}"
+        errors << p.phone
+      end
+    end
+    errors
+  end
 end
